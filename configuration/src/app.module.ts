27458dd522c8d40e2956ebb53ccd9d76301a6b06
nemobaji <khw175@naver.com>
2026-02-05 22:16:11 +0900

    Add: TECHNIQUES/Configuration

--- configuration/src/app.module.ts
+++ configuration/src/app.module.ts
@@ -7,7 +7,7 @@ import configuration from './configuration/configuration';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // true로 설정하지 않을 경우, 이를 사용할 모듈에 ConfigModule을 임포트해야합니다.
-      load: [configuration],
+      load: [configuration], // 커스텀 설정 파일을 사용하기 위해 load 속성에 파일을 명시합니다.
     }),
     ConditionalModule.registerWhen(ConfigurationModule, 'USE_FOO'), // 환경 변수의 boolean 값에 따라 모듈의 등록 여부를 결정합니다.
   ],
